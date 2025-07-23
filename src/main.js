209ad39f6e90299897d2f43bf6203a791db2b253
darellanodev <darellano.aliaga@gmail.com>
2025-07-23 18:06:29 +0200

    refactor: Extract JS code from HTML (#1)

---
+++ src/main.js
@@ -0,0 +1,12 @@
+(async () => {
+  // Gemini 2.0 Flash
+  const chat_resp = await puter.ai.chat(
+    "Generate a recipe of spanish omelette",
+    true,
+    { model: "gemini-2.0-flash", stream: true }
+  );
+  puter.print("<h1>Gemini 2.0 Flash:</h1>");
+  for await (const part of chat_resp) {
+    puter.print(part?.text?.replaceAll("\n", "<br>"));
+  }
+})();
