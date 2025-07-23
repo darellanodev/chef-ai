433da574461cade51045089d326696d9d3d10a88
darellanodev <darellano.aliaga@gmail.com>
2025-07-23 18:10:45 +0200

    feat: Add image generated with DALL-E 3 (closes #1)

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 (async () => {
-  // Gemini 2.0 Flash
+  // Generate text with Gemini 2.0 Flash
   const chat_resp = await puter.ai.chat(
     "Generate a recipe of spanish omelette",
     true,
@@ -9,4 +9,9 @@
   for await (const part of chat_resp) {
     puter.print(part?.text?.replaceAll("\n", "<br>"));
   }
+
+  // Generate image with DALL-E 3
+  puter.ai.txt2img("A picture of a cat.", true).then((image) => {
+    document.body.appendChild(image);
+  });
 })();
