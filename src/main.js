83920725e8e06d308a2a29c00c268215caacddd9
darellanodev <darellano.aliaga@gmail.com>
2025-07-27 22:31:57 +0200

    feat: Create a button to call AI (#2)

--- src/main.js
+++ src/main.js
@@ -1,17 +1,24 @@
-(async () => {
+const resultContainer = document.getElementById("result");
+
+async function generateRecipe() {
+  resultContainer.innerHTML = "";
   // Generate text with Gemini 2.0 Flash
   const chat_resp = await puter.ai.chat(
     "Generate a recipe of spanish omelette",
     true,
     { model: "gemini-2.0-flash", stream: true }
   );
-  puter.print("<h1>Gemini 2.0 Flash:</h1>");
+  resultContainer.innerHTML = "<h1>Gemini 2.0 Flash:</h1>";
   for await (const part of chat_resp) {
-    puter.print(part?.text?.replaceAll("\n", "<br>"));
+    resultContainer.innerHTML += part?.text?.replaceAll("\n", "<br>");
   }
 
   // Generate image with DALL-E 3
   puter.ai.txt2img("A picture of a cat.", true).then((image) => {
-    document.body.appendChild(image);
+    resultContainer.appendChild(image);
   });
-})();
+}
+
+document
+  .getElementById("generateRecipe")
+  .addEventListener("click", generateRecipe);
