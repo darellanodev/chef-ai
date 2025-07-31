d5fb044ffcf0e5b6e64916f3eee73f765dd7f22b
darellanodev <darellano.aliaga@gmail.com>
2025-07-31 20:35:06 +0200

    feat: Get the correct response depending on tesMode value (#2)

--- src/main.js
+++ src/main.js
@@ -1,15 +1,17 @@
 const resultContainer = document.getElementById("result");
 
 async function generateTextRecipe() {
+  const testMode = true;
   const chat_resp = await puter.ai.chat(
     "Generate a recipe of spanish omelette",
-    true,
-    { model: "gemini-2.0-flash", stream: true }
+    testMode,
+    { model: "gemini-2.0-flash" }
   );
-  resultContainer.innerHTML = "<h1>Gemini 2.0 Flash:</h1>";
-  for await (const part of chat_resp) {
-    resultContainer.innerHTML += part?.text?.replaceAll("\n", "<br>");
-  }
+  console.log(chat_resp);
+  let response = testMode
+    ? chat_resp.message.content[0].text
+    : chat_resp.message.content;
+  resultContainer.innerHTML += response.replaceAll("\n", "<br>");
 }
 
 async function generateImageRecipe() {
