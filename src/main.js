ce730c789583d7987a8e29c1b9bee78de693b49b
darellanodev <darellano.aliaga@gmail.com>
2025-07-31 20:45:05 +0200

    feat: Create a prompt to generate a JSON format response (closes #2)

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 const resultContainer = document.getElementById("result");
 
 async function generateTextRecipe() {
-  const testMode = true;
+  const testMode = false;
+  const userRecipe = "spanish omelette";
   const chat_resp = await puter.ai.chat(
-    "Generate a recipe of spanish omelette",
+    `Generate a strictly JSON format about a '${userRecipe}' cooking recipe where there are two mainly keys: ingredients and instructions. Both of them are an array of strings. You must be concise in your responses.`,
     testMode,
     { model: "gemini-2.0-flash" }
   );
