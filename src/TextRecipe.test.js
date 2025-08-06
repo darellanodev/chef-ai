696dee99148c6967172fc02a3ed0813834e7adbe
darellanodev <darellano.aliaga@gmail.com>
2025-08-06 17:57:45 +0200

    Refactor: Extract textRecipe instantiation (#3)

--- src/TextRecipe.test.js
+++ src/TextRecipe.test.js
@@ -1,27 +1,23 @@
 import { it, expect, describe } from "vitest";
 import TextRecipe from "./TextRecipe.js";
 
+const textRecipe = new TextRecipe(null, "spanish omelette", false);
 describe("TextRecipe - validate method", () => {
   it("should return false when responseAI is an empty string", () => {
-    const textRecipe = new TextRecipe(null, "spanish omelette", false);
     expect(textRecipe.validate("")).toBe(false);
   });
   it("should return true when responseAI is a valid JSON", () => {
-    const textRecipe = new TextRecipe(null, "spanish omelette", false);
     const responseAI =
       '{ "ingredients": ["eggs", "potatoes"], "instructions": ["mix", "cook"] }';
     expect(textRecipe.validate(responseAI)).toBe(true);
   });
   it("should return false when responseAI is a valid JSON but it does not have the keys ingredients and instructions", () => {
-    const textRecipe = new TextRecipe(null, "spanish omelette", false);
-
     const responseAI =
       '{ "items": ["eggs", "potatoes"], "steps": ["mix", "cook"] }';
 
     expect(textRecipe.validate(responseAI)).toBe(false);
   });
   it("should return false when ingredients and instructions are not arrays of strings", () => {
-    const textRecipe = new TextRecipe(null, "spanish omelette", false);
     const responseAI =
       '{ "ingredients": "eggs", "instructions": ["mix", "cook"] }';
 
