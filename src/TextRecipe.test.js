8df0afc93686cd3996dd47a2efb59cb78deef14b
darellanodev <darellano.aliaga@gmail.com>
2025-08-06 17:18:15 +0200

    fix: Correct typo in text (#3)

--- src/TextRecipe.test.js
+++ src/TextRecipe.test.js
@@ -1,7 +1,7 @@
 import { it, expect, describe } from "vitest";
 import TextRecipe from "./TextRecipe.js";
 
-describe("TextRecipe - validate methods", () => {
+describe("TextRecipe - validate method", () => {
   it("should return false when responseAI is an empty string", () => {
     const textRecipe = new TextRecipe(null, "spanish omelette", false);
     expect(textRecipe.validate("")).toBe(false);
