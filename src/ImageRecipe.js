d2063a77187b60257009cabb77cd6c207824cbd7
darellanodev <darellano.aliaga@gmail.com>
2025-08-30 15:42:07 +0200

    fix: Use the testMode property (#5)

--- src/ImageRecipe.js
+++ src/ImageRecipe.js
@@ -5,7 +5,7 @@ export default class ImageRecipe {
   }
   async generateImageRecipe(imageElement) {
     puter.ai
-      .txt2img(`A picture of a recipe for a ${this.userRecipe}.`, true)
+      .txt2img(`A picture of a recipe for a ${this.userRecipe}.`, this.testMode)
       .then((image) => {
         imageElement.appendChild(image)
       })
