488a52fc306cf50985e5a12a3f4b21c6eebcb045
darellanodev <darellano.aliaga@gmail.com>
2025-08-30 19:10:56 +0200

    feat: Show and hide the spinner and the recipe card (#7)

--- src/main.js
+++ src/main.js
@@ -6,9 +6,13 @@ const imageElement = document.getElementById('image')
 const ingredientsElement = document.getElementById('ingredients')
 const instructionsElement = document.getElementById('instructions')
 const errorMessage = document.getElementById('errorMessage')
+const userRecipeElement = document.getElementById('userRecipe')
+const spinnerElement = document.getElementById('spinner')
+const recipeCardElement = document.getElementById('recipeCard')
 
 async function generateRecipe() {
-  const userRecipe = 'spanish omelette'
+  spinnerElement.classList.remove('hide')
+  const userRecipe = userRecipeElement.value
   const textRecipe = new TextRecipe(userRecipe, textTestMode)
   const imageRecipe = new ImageRecipe(userRecipe, imageTestMode)
   imageElement.innerHTML = ''
@@ -24,6 +28,8 @@ async function generateRecipe() {
   } else {
     errorMessage.innerHTML = 'Error, try again.'
   }
+  spinnerElement.classList.add('hide')
+  recipeCardElement.classList.remove('hide')
 }
 
 document
