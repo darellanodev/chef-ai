de11c263b4f96d6a40afb46469471bf897e0b966
darellanodev <darellano.aliaga@gmail.com>
2025-09-01 21:32:03 +0200

    refactor: Extract RecipeDOM class (#7)

--- src/main.js
+++ src/main.js
@@ -1,35 +1,30 @@
+import RecipeDOM from './RecipeDOM.js'
 import TextRecipe from './TextRecipe.js'
 import ImageRecipe from './ImageRecipe.js'
 import { textTestMode, imageTestMode } from './config.js'
 
-const imageElement = document.getElementById('image')
-const ingredientsElement = document.getElementById('ingredients')
-const instructionsElement = document.getElementById('instructions')
-const errorMessage = document.getElementById('errorMessage')
-const userRecipeElement = document.getElementById('userRecipe')
-const spinnerElement = document.getElementById('spinner')
-const recipeCardElement = document.getElementById('recipeCard')
+const dom = new RecipeDOM()
 
 async function generateRecipe() {
-  spinnerElement.classList.remove('hide')
-  const userRecipe = userRecipeElement.value
+  dom.spinnerElement.classList.remove('hide')
+  const userRecipe = dom.userRecipeElement.value
   const textRecipe = new TextRecipe(userRecipe, textTestMode)
   const imageRecipe = new ImageRecipe(userRecipe, imageTestMode)
-  imageElement.innerHTML = ''
+  dom.imageElement.innerHTML = ''
   let response = await textRecipe.getAI()
   response = textRecipe.clean(response)
   if (textRecipe.validate(response)) {
     textRecipe.print(
       JSON.parse(response),
-      ingredientsElement,
-      instructionsElement
+      dom.ingredientsElement,
+      dom.instructionsElement
     )
-    await imageRecipe.generateImageRecipe(imageElement)
+    await imageRecipe.generateImageRecipe(dom.imageElement)
   } else {
-    errorMessage.innerHTML = 'Error, try again.'
+    dom.errorMessage.innerHTML = 'Error, try again.'
   }
-  spinnerElement.classList.add('hide')
-  recipeCardElement.classList.remove('hide')
+  dom.spinnerElement.classList.add('hide')
+  dom.recipeCardElement.classList.remove('hide')
 }
 
 document
